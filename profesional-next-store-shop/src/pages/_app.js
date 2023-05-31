cf56c327efd969c90d47c8f6c3742dfbf9ac92d3
jandres-vega <jandresvega@itsa.edu.co>
2023-05-30 19:57:22 -0500

    Feact add project profesional en next

---
+++ profesional-next-store-shop/src/pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/tailwind.css";
+import MainLayout from "@layout/MainLayout";
+export default function App({ Component, pageProps }) {
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
+}
